c0c5a6fbcee9a46d9b69ce76f6bf75bdd54df800
Adama Jallow <hncb7fzqzr@privaterelay.appleid.com>
2026-05-30 14:54:21 +0000

    Added input validation and duplicate prevention to CLI

--- main.py
+++ main.py
@@ -16,20 +16,42 @@ def main():
 
         choice = input("\nEnter your pick: ")
         if choice == "1":
-            name = input("Enter habit name: ")
-            periodicity = input("Enter periodicity (daily/weekly): ")
+            while True:
+                name = input("Enter habit name: ").strip()
+                if name:
+                    break
+                else:
+                    print("Habit name cannot be empty. Please enter a valid name.") 
+            while True:
+                 periodicity = input("Enter periodicity (daily/weekly): ").lower()
+                 if periodicity in ["daily", "weekly"]:
+                     break
+                 else:
+                     print("Invalid periodicity. Please enter 'daily' or 'weekly'.")
             manager.create_habit(name, periodicity)
 
         elif choice == "2":
-            name = input("Enter the habit you want to delete: ")
+            while True:
+              name = input("Enter the habit you want to delete: ").strip()
+              if name:
+                  break
+              else:
+                  print("Habit name cannot be empty. Please enter a valid name.")
             manager.delete_habit(name)
+
         elif choice == "3":
-            name = input("Enter completed habit: ")
+            while True:
+                name = input("Enter completed habit: ").strip()
+                if name:
+                    break
+                else:
+                    print("Habit name cannot be empty. Please enter a valid name.")
             for habit in manager.habits:
                 if habit.name == name:
                     habit.mark_complete()
                     manager.save_habits()
                     break
+                
         elif choice == "4":
             habits = analytics.list_all_habits(manager.habits)
             for habit in habits:
@@ -37,10 +59,16 @@ def main():
             input("\nPress Enter to continue...")    
             
         elif choice == "5":
-            periodicity = input("Enter periodicity (daily/weekly): ")
+            while True:
+                 periodicity = input("Enter periodicity (daily/weekly): ").lower()
+                 if periodicity in ["daily", "weekly"]:
+                     break
+                 else:
+                     print("Invalid periodicity. Please enter 'daily' or 'weekly'.")   
             habits = analytics.filter_by_periodicity(manager.habits, periodicity)
             for habit in habits:
                 print(habit.name)
+
         elif choice == "6":
             streak = analytics.get_longest_streak_all(manager.habits)
             print(f"highest streak: {streak}")
